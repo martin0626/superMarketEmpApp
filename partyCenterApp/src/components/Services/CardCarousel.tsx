44d7735f24ff719178fd71e41f869970746a71a7
martin0626 <scbsteel12@gmail.com>
2025-10-28 17:53:59 +0200

    Added Prices Section

--- partyCenterApp/src/components/Services/CardCarousel.tsx
+++ partyCenterApp/src/components/Services/CardCarousel.tsx
@@ -1,78 +1,65 @@
+// import { useState } from "react";
 import Slider from "react-slick";
+import { type priceCardT } from "./Services";
 
-export default function CardCarousel() {
+
+type cardCarouselPropsT = {
+  cards: priceCardT[],
+  title: string
+}
+
+export default function CardCarousel({cards, title}: cardCarouselPropsT) {
+
+  // const [activeSlide, setActiveSlide] = useState(0);
+
   const settings = {
     customPaging: function(i: any) {
       return (
-        <a>
-          Page
+        <a className={"button-primary-outlined-prices"}>
+          {cards[i].btnName}
         </a>
       );
     },
     dots: true,
-    dotsClass: "slick-dots slick-thumb",
+    // dotsClass: "slick-dots",
     infinite: true,
     speed: 500,
     slidesToShow: 1,
-    slidesToScroll: 1
+    slidesToScroll: 1,
   };
 
 
   return (
     <section className="prices-slider-container">
+      <h1>{title}</h1>
       <Slider {...settings}>
-        <div className="price-card">
-          <div className="price-card-iamge-holder">
-            <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlDtui1l97Pxeh_senc2eckSIHJg8CGCZj5Q&s"} />
-          </div>
-          <div className="price-card-text-holder">
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad aspernatur quos ratione eius commodi 
-              dolore distinctio perferendis fuga veritatis nam quo dolor facere placeat corrupti quam, saepe aliquam mollitia nesciunt.
-            </p>
-          </div>
-        </div>
-        
+        {
+          cards.map((card, index)=><div
+                      key={index}
+                      className="package-card-prices"
+                    >
+                      <div className="package-prices-image">
+                        <img src={card.img} alt={card.title} />
+                      </div>
 
-        <div className="price-card">
-          <div className="price-card-iamge-holder">
-            <img src={"https://ballooncoach.com/wp-content/uploads/2014/12/Pool-Graduation-Wrap-and-both-Arcs.jpg"} />
-          </div>
-          <div className="price-card-text-holder">
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad aspernatur quos ratione eius commodi 
-              dolore distinctio perferendis fuga veritatis nam quo dolor facere placeat corrupti quam, saepe aliquam mollitia nesciunt.
-            </p>
-          </div>
-        </div>
+                      <div className="package-prices-content">
+                        <h3>{card.title}</h3>
+                        <p>{card.description}</p>
+                        <h4>Ценa:</h4>
 
-
-
-        <div className="price-card">
-          <div className="price-card-iamge-holder">
-            <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlDtui1l97Pxeh_senc2eckSIHJg8CGCZj5Q&s"} />
-          </div>
-          <div className="price-card-text-holder">
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad aspernatur quos ratione eius commodi 
-              dolore distinctio perferendis fuga veritatis nam quo dolor facere placeat corrupti quam, saepe aliquam mollitia nesciunt.
-            </p>
-          </div>
-        </div>
-
-
-        <div className="price-card">
-          <div className="price-card-iamge-holder">
-            <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlDtui1l97Pxeh_senc2eckSIHJg8CGCZj5Q&s"} />
-          </div>
-          <div className="price-card-text-holder">
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad aspernatur quos ratione eius commodi 
-              dolore distinctio perferendis fuga veritatis nam quo dolor facere placeat corrupti quam, saepe aliquam mollitia nesciunt.
-            </p>
-          </div>
-        </div>
-
+                        <div className="package-prices-content-price">
+                          <p>От <span>{card.price}</span> лв.</p>
+                          <p>До <span>{card.price * 2}</span> лв.</p>
+                        </div>
+                        <button
+                          className="button-primary"
+                        >
+                          Заяви сега
+                        </button>
+                      </div>
+                    </div>
+          )
+        } 
       </Slider>
     </section>
   );
--- partyCenterApp/src/components/Services/Services.tsx
+++ partyCenterApp/src/components/Services/Services.tsx
@@ -1,16 +1,50 @@
+import { title } from "framer-motion/client";
 import CardCarousel from "./CardCarousel";
 import Pricing from "./Pricing";
 import PricingCard from "./PricingCard";
 
+
+export type priceCardT = {
+  title: string,
+  btnName: string,
+  price: number,
+  description: string,
+  img: string
+}
+
+
 export default function Services() {
 
+  let cards: priceCardT[] = [
+    {
+      title: "Малка Арка",
+      btnName: "Малка",
+      price: 100,
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui doloremque architecto deserunt dolorem ut nam reprehenderit eligendi animi natus culpa magnam, id eaque, doloribus aperiam quam maiores sequi sint tenetur.",
+      img: 'https://ae01.alicdn.com/kf/S1203565424ea4a788d22cc24ca1ae72bk.jpg_640x640q90.jpg'
+    },
+    {
+      title: "Голяма Арка",
+      btnName: "Голяма",
+      price: 100,
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui doloremque architecto deserunt dolorem ut nam reprehenderit eligendi animi natus culpa magnam, id eaque, doloribus aperiam quam maiores sequi sint tenetur.",
+      img: 'https://images.ctfassets.net/77l22z9el0aa/58YaDaZF4Gc2nG6eIXLozF/50d932c299d97b533bd122c3d59446fa/inline_shutterstock.png'
+    },
+    {
+      title: "Кръгла Арка",
+      btnName: "Кръгла",
+      price: 100,
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui doloremque architecto deserunt dolorem ut nam reprehenderit eligendi animi natus culpa magnam, id eaque, doloribus aperiam quam maiores sequi sint tenetur.",
+      img: 'https://www.elliesparty.com/cdn/shop/products/boho-safari-balloon-arch-sage-gold-balloon-garland-kit-387987.jpg?v=1687470121&width=1100'
+    }
+  ]
 
-  
   return (
     <section className="show-top">
         {/* <Pricing/> */}
         {/* <PricingCard/> */}
-        <CardCarousel/>
+        <CardCarousel cards={cards} title={"Нашите Арки"}/>
+        <CardCarousel cards={cards} title={"Украса за повод"}/>
     </section>
   );
 }
